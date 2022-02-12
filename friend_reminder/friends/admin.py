9fa41683a13ca8dfb0244708aceb52efb1ce8ac1
Siwon  Kim <siwon@usf.edu>
2022-02-12 14:20:13 -0500

    User registration Section

--- friend_reminder/friends/admin.py
+++ friend_reminder/friends/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from friends import models
+
 # Register your models here.
+admin.site.register(models.UserProfile)
--- friend_reminder/friends/models.py
+++ friend_reminder/friends/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileSuperUser(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email,)
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser with given details"""
+        user = self.create_user(email, password)
+
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model for users in the system"""
+    email = models.EmailField(max_length=255, unique=True)
+    birthday=models.DateField(auto_now=False, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileSuperUser()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELD = ['email', 'birthday']
+
+    def ___str___(self):
+        return self.email
